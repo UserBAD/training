86d1056bd8d0e4b328359c36229000f242f7c50a
userAD <busygin_ad@mail.ru>
2021-07-26 04:47:51 +0400

    Homework 11

---
+++ src/main/java/lesson11_dz_generics/Fruit.java
@@ -0,0 +1,14 @@
+package lesson11_dz_generics;
+
+public class Fruit {
+    private String type;
+    private float weight;
+    public Fruit ( String type, float weight){
+        this.type=type;
+        this.weight=weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ src/main/java/lesson11_dz_generics/Orange.java
@@ -0,0 +1,6 @@
+package lesson11_dz_generics;
+
+public class Orange extends Fruit{
+    public Orange (){super("Orange",1.5f);}
+
+}
